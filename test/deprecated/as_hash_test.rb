6860cb26de2ebbc87007c046934ec26fa5108fcb
Greg Clarke <gregbclarke@gmail.com>
2017-07-27 12:42:30 +1000

    Changed the test for deprecated as_hash to use the capture-out gem instead of capture_io and it now should pass.

--- test/deprecated/as_hash_test.rb
+++ test/deprecated/as_hash_test.rb
@@ -3,9 +3,12 @@ require "test_helper"
 class AsHashTest < Test::Unit::TestCase
   def test_as_hash
     p = Dish({ a: 1, b: 2 })
-
-    out, err = capture_io do
-      assert_equal({ "a" => 1, "b" => 2}, p.as_hash)
+ 
+    err = nil
+    out = Capture.stdout do
+	    err = Capture.stderr do
+        assert_equal({ "a" => 1, "b" => 2}, p.as_hash)
+	    end
     end
 
     assert_match /Dish::Plate#as_hash is deprecated\./, err
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,4 @@
 require "test/unit"
 require "dish/ext"
+require "capture-output"
+
